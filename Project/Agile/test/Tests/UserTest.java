b2f6406ba1e0019ca70a80d5226c0f0346fd07db
Carim A <carim.akeju@glaciate.net>
2017-02-21 13:02:24 +0000

    User - Test class

--- Project/Agile/test/Tests/UserTest.java
+++ Project/Agile/test/Tests/UserTest.java
@@ -3,10 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-import static org.junit.Assert.*;
-import org.junit.Test;
-
 package Tests;
+
+import org.junit.Test;
+import org.junit.Assert.*;
 
 /**
  *
