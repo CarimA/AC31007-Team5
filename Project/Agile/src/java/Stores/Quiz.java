ecb9e3c4da6cd138cf51c2841607999535dd1164
jimiwesterholm <jimi.westerholm@gmail.com>
2017-02-22 13:06:28 +0000

    quiz model added
    for taking a quiz

--- Project/Agile/src/java/Stores/Quiz.java
+++ Project/Agile/src/java/Stores/Quiz.java
@@ -5,6 +5,10 @@
  */
 package Stores;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Date;
 import java.util.List;
 import java.util.UUID;
@@ -19,18 +23,18 @@ public class Quiz {
     String title;
     String module;
     Date dateCreated;
-    boolean avaliable;
+    boolean available;
     List<Question> questions;
 
     public Quiz() {
     }
 //
-    public Quiz(int id, String title, String module, Date dateCreated, boolean avaliable, List<Question> questions) {
+    public Quiz(int id, String title, String module, Date dateCreated, boolean available, List<Question> questions) {
         this.id = id;
         this.title = title;
         this.module = module;
         this.dateCreated = dateCreated;
-        this.avaliable = avaliable;
+        this.available = available;
         this.questions = questions;
     }
 
@@ -58,12 +62,12 @@ public class Quiz {
         this.dateCreated = dateCreated;
     }
 
-    public boolean isAvaliable() {
-        return avaliable;
+    public boolean isAvailable() {
+        return available;
     }
 
-    public void setAvaliable(boolean avaliable) {
-        this.avaliable = avaliable;
+    public void setAvailable(boolean available) {
+        this.available = available;
     }
    
     public int getId() {
@@ -80,5 +84,41 @@ public class Quiz {
 
     public void setQuestions(List<Question> questions) {
         this.questions = questions;
+    }
+    
+    public static Quiz fetch(Connection connection, int id) throws SQLException {
+        Quiz q = new Quiz();
+        
+        PreparedStatement statement = connection.prepareStatement("SELECT * FROM quiz where qId = ?");
+        statement.setInt(1, id);
+        ResultSet rs = statement.executeQuery();
+          
+        if (!rs.next()) {
+            System.out.println("Quiz not found");
+        }
+        
+        String title = rs.getString("Title");
+        String module = rs.getString("Module");
+        Date dc = rs.getDate("DateCreated");
+        int available = rs.getInt("Available");
+        
+        
+        if (available == 0) {
+            q.setAvailable(false);
+        } else {
+            q.setAvailable(true);
+        }
+        
+        q.setId(id);
+        q.setDateCreated(dc);
+        q.setModule(module);
+        q.setTitle(title);
+        
+        
+        rs.close();
+        statement.close();
+        connection.close();
+        
+        return q;
     }
 }
