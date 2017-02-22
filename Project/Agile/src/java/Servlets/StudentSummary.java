37b7ae4619b0b54879fcf8d1bfcae9957fdcd38e
ATarasovs <a.tarasov1209@gmail.com>
2017-02-22 13:16:12 +0000

    Print and send email buttons work properly

--- Project/Agile/src/java/Servlets/StudentSummary.java
+++ Project/Agile/src/java/Servlets/StudentSummary.java
@@ -35,6 +35,19 @@ import java.sql.SQLException;
 
 
 /**
+ * libraries for sending mail
+ */
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+
+/**
  *
  * @author Aleksandr
  */
@@ -58,7 +71,7 @@ public class StudentSummary extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        RequestDispatcher rd = request.getRequestDispatcher("summary.jsp");
+        RequestDispatcher rd = request.getRequestDispatcher("studentsummary.jsp");
         rd.forward(request, response);
     }
 
@@ -75,7 +88,8 @@ public class StudentSummary extends HttpServlet {
             throws ServletException, IOException {
         RequestDispatcher rd;
         //HttpSession session = request.getSession();
-        rd = request.getRequestDispatcher("studentsummary.jsp");
+        rd = request.getRequestDispatcher("summarysent.jsp");
+        sendEmail();
         rd.forward(request, response);
         
         //String id = request.getParameter("id");
@@ -101,6 +115,43 @@ public class StudentSummary extends HttpServlet {
         //}
         //rd.forward(request, response);
         
+    }
+    
+    public void sendEmail()
+    {
+        final String username = "agileteam5email@gmail.com";
+        final String password = "Qwerty12345";
+
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+
+        Session session = Session.getInstance(props,
+          new javax.mail.Authenticator() {
+                protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username, password);
+                }
+          });
+
+        try {
+
+                Message message = new MimeMessage(session);
+                message.setFrom(new InternetAddress("agileteam5email@gmail.com"));
+                message.setRecipients(Message.RecipientType.TO,
+                        InternetAddress.parse("a.tarasov1209@gmail.com"));
+                message.setSubject("Testing Subject");
+                message.setText("Dear Mail Crawler,"
+                        + "\n\n No spam to my email, please!");
+
+                Transport.send(message);
+
+                System.out.println("Done");
+
+        } catch (MessagingException e) {
+                throw new RuntimeException(e);
+        }
     }
 //    public void createPDF(String id, String firstname, String lastname, String module, String filepathname) throws IOException
 //    {
---
+++ Project/Agile/web/summarysent.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : summarysent
+    Created on : Feb 22, 2017, 1:09:13 PM
+    Author     : Aleksandr
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>The summary was successfully sent</h1>
+    </body>
+</html>
