9a92b77e0cc52bcba0a68157289a8f94ff89db1c
Dan Ward <dan.steve@hotmail.co.uk>
2017-02-23 18:57:14 +0000

    quizzes on overviewn now

---
+++ Project/Agile/src/java/Servlets/QuizOverview.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Servlets;
+
+import Models.DBConnect;
+import Stores.Quiz;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Vector;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Danstev
+ */
+@WebServlet(name = "QuizOverview", urlPatterns = {"/QuizOverview"})
+public class QuizOverview extends HttpServlet {
+
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        DBConnect d = new DBConnect();
+        Vector<Quiz> quizzes = d.getQuizzes("Select * from Quiz");
+        RequestDispatcher rd = request.getRequestDispatcher("/QuizOverview.jsp");
+        request.setAttribute("quizzes", quizzes);
+        rd.forward(request,response);
+        
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+    }
+
+}
