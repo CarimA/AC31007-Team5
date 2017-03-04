33b19e6368b96deaaad4c2e3cb42d8ee223fec8d
IBogd <ibogdanov888@gmail.com>
2017-03-04 18:10:07 +0000

    more tests

---
+++ Project/Agile/test/Models/LoginModelIT.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Models;
+
+
+import Exception.UsernameInvalidException;
+import Stores.User;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Igors Bogdanovs <i.bogdanovs@dundee.ac.uk>
+ */
+public class LoginModelIT {
+    
+    public LoginModelIT() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * NEET TO DO MORE CHECKS
+     * Test of checkLogin method, of class LoginModel.
+     * @throws java.lang.Exception
+     */
+     @Test //(expected = UsernameInvalidException.class)
+    public void testCheckLogin() throws Exception {
+        System.out.println("checkLogin");
+        String username = "13";
+        String password = "password";
+        LoginModel instance = new LoginModel();
+        User expResult = new User();
+        User result = instance.checkLogin(username, password);
+        assertEquals(expResult, result);
+        
+    }
+
+    /**
+     * Test of registerUser method, of class LoginModel.
+     */
+    @Test
+    public void testRegisterUser() {
+        System.out.println("registerUser");
+        String userID = "123456712";
+        String password = "password";
+        String displayName = "admin";
+        String email = "123456712@gmail.com";
+        String position = "staff";
+        LoginModel instance = new LoginModel();
+        User expResult = null;
+        User result = instance.registerUser(userID, password, displayName, email, position);
+        assertEquals(expResult, result);
+       
+    }
+    
+}
