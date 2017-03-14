2c35159a064689aae35919bfba1b6305132a857b
jimiwesterholm <jimi.westerholm@gmail.com>
2017-03-14 12:11:57 +0000

    Staff results page stuff + tests
    For viewing student's results

---
+++ Project/Agile/test/Misc/ResultControllerTest.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Misc;
+
+import Stores.ResultModel;
+import java.util.List;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author jimiwesterholm
+ */
+public class ResultControllerTest {
+    
+    public ResultControllerTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testFetchResultsOverview() {
+        int expectedResult = 12;
+        ResultController rc = new ResultController();
+        List<ResultModel> results = rc.fetchResultOverview();
+        assertEquals(expectedResult, results.get(0).getId());
+    }
+    
+}
