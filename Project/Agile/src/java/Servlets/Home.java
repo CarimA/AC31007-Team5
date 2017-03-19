03ee1ac6e0ff2f56111502551c48e97871223dfb
Carim A <carim.akeju@glaciate.net>
2017-03-19 13:48:16 +0000

    Moving servlets

--- Project/Agile/src/java/Servlets/Home.java
+++ Project/Agile/src/java/Servlets/Home.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Servlets.newServlets;
+package Servlets;
 
 import Exception.NotImplementedException;
 import Misc.Helpers;
