85242b66d1ce72a69aed59896937cb426713a3c2
aleksandrstarasovs <aleksandrstarasovs@labpc251.computing.dundee.ac.uk>
2017-03-20 16:05:15 +0000

    StudentSummary changes

--- Project/Agile/src/java/Servlets/StudentSummary.java
+++ Project/Agile/src/java/Servlets/StudentSummary.java
@@ -84,24 +84,53 @@ public class StudentSummary extends HttpServlet {
         String id = user.getId();
         
         Stores.Quiz quiz = (Stores.Quiz) (request.getSession().getAttribute("quiz"));
+        String title = quiz.getTitle();
+        String module = quiz.getModule();
         List<Question> questions = quiz.getQuestions();
         
-        List<Answer> answer = (List<Answer>) (request.getSession().getAttribute("answer"));
-        
+        List<Answer> answersStudent = (List<Answer>) (request.getSession().getAttribute("studentAns"));
+        List<Answer> answers = new ArrayList<Answer>();
  
 
        // String[] studentAnswers = request.getParameterValues("studentanswers");
         //String[] correctAnswers = request.getParameterValues("correctanswers");
         String questionss = "";
-        String studentAnswerss = "";
+        String studentAnswers = "";
         String check = "";
         int count = 1;
         String q ="";
+        String a = "";
+        String correct = "";
+        int p;
+        int points = 0;
         
         for (int i=0; i<questions.size(); i++)
         {
             q = questions.get(i).getQuestion();
-            //if(correctAnswers[i].equals(studentAnswers[i]))
+            p = questions.get(i).getPoints();
+            answers = questions.get(i).getAnswers();
+            a = answers.get(i).getAnswer();
+            
+            if(answersStudent.get(i).isRight())
+            {
+                check = "right";
+                correct = a;
+                points += p;
+                
+            }
+            else 
+            {
+                check = "false";
+                for (int u=0; u<answers.size(); u++)
+                {
+                    if (answers.get(u).isRight()) 
+                    {
+                        correct = answers.get(u).getAnswer();
+                    }
+                }
+            }
+ 
+            
             //{
                //check = "right";
             //}
@@ -110,9 +139,8 @@ public class StudentSummary extends HttpServlet {
                 //check = "false";
             //}
                 questionss += ("Your answer for the Question " + count + " is " + check 
-                        + "\n\n" + "Question " + count + ": " + q + "\n" + "Your answer: " //+ studentAnswers[i]
-                         + "\n" + "Correct answer: " + //correctAnswers[i]+ 
-                        "\n\n");
+                        + "\n\n" + "Question " + count + ": " + q + "\n" + "Your answer: " + a
+                         + "\n" + "Correct answer: " + correct + "\n\n");
                 count+=1;
 
 
@@ -122,21 +150,20 @@ public class StudentSummary extends HttpServlet {
         }
         
         String plaintext = ("Hello " + displayname + " (Matric No: " + id + "), \n\n"
-                + "Your overall score for the Quiz " + //quizid + 
-                " is " + //score + 
-                " points. \n"
+                + "Your overall score for the " + title + 
+                " quiz is " + points + " points. \n"
                 + "\n\nDescription \n***********************************************************************\n"
                 + questionss + "***********************************************************************\n\n"
                 + "Thank you for participation! \n\nRegards, \nYour Agile Team 5");
         
-        sendEmail(email, plaintext);
+        sendEmail(email, plaintext, title);
         System.out.println(plaintext);
         
         rd = request.getRequestDispatcher("summarysent.jsp");
         rd.forward(request, response);
     }
     
-    public void sendEmail(String email, String plaintext) throws FileNotFoundException
+    public void sendEmail(String email, String plaintext, String title) throws FileNotFoundException
     { 
         final String username = "agileteam5email@gmail.com";
         final String password = "Qwerty12345";
@@ -157,7 +184,7 @@ public class StudentSummary extends HttpServlet {
                 message.setFrom(new InternetAddress("agileteam5email@gmail.com"));
                 message.setRecipients(Message.RecipientType.TO,
                 InternetAddress.parse(email));
-                message.setSubject("Quiz summary for quiz "); 
+                message.setSubject("Quiz summary for " + title); 
                         //quizid);
                // StringWriter writer = new StringWriter();
                 //IOUtils.copy(new FileInputStream(new File("processResults.jsp")), writer);
